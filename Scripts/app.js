07960ed5a74cb6136d57231510e9fe859693ddc7
mrkeyboard44 <matthewdandar@gmail.com>
2021-04-21 14:27:01 -0700

    final submission

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,18 +6,39 @@ const albums = [
 ];
 
 function defineAddItemSection() {
-    html = '<div class="cart-row">\
-        <span>IMAGE</span>\
-        <span>NAME</span>\
-        <span>PRICE</span>\
-        </div>'
+    html = '<div class="cart-item-row"></div>'
     return html
 }
+function defineAddItem(albumNumber) {
+    html = '<span>IMAGE</span>\
+        <span>' + albums[albumNumber]['name'] + '</span>\
+        <span>' + albums[albumNumber]['price'] + '</span>'
+    return html
+}
+
+const cartSection = document.querySelector('.cart')
+        cartSection.insertAdjacentHTML('afterbegin', defineAddItemSection())
 
 function addItemToCart(albumNumber) {
     if (albumNumber == 1) {
         console.log(albums[0])
-        const cartSection = document.querySelector('.cart')
-        cartSection.insertAdjacentHTML('afterbegin', defineAddItemSection())
+        const cartSection = document.querySelector('.cart-item-row')
+        cartSection.insertAdjacentHTML('afterend', defineAddItem(0))
+        albums[0]['inCart'] = true;
+    }
+    if (albumNumber == 2) {
+        console.log(albums[1])
+        const cartSection = document.querySelector('.cart-item-row')
+        cartSection.insertAdjacentHTML('afterend', defineAddItem(1))
+    }
+    if (albumNumber == 3) {
+        console.log(albums[2])
+        const cartSection = document.querySelector('.cart-item-row')
+        cartSection.insertAdjacentHTML('afterend', defineAddItem(2))
+    }
+    if (albumNumber == 4) {
+        console.log(albums[3])
+        const cartSection = document.querySelector('.cart-item-row')
+        cartSection.insertAdjacentHTML('afterend', defineAddItem(3))
     }
 }

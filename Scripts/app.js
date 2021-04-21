5d551e4831cf9dceb521e53cb87e44f684187d9b
mrkeyboard44 <matthewdandar@gmail.com>
2021-04-21 13:47:39 -0700

    added addItemToCart function

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,4 +3,10 @@ const albums = [
     { name: "Album 2", price: 14.99, inCart: false }, 
     { name: "Album 3", price: 9.99, inCart: false }, 
     { name: "Album 4", price: 19.99, inCart: false }
-  ];
+    ];
+
+function addItemToCart(albumNumber) {
+    if (albumNumber == 1) {
+        console.log(albums[0])
+    }
+}

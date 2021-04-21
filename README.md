e1cffd8061675e0dfa1d978f13b65fb70e3920c2
mrkeyboard44 <matthewdandar@gmail.com>
2021-04-21 13:37:06 -0700

    added ablums const

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Exam-1620
+# Exam-1620 By Matthew Dandar A01180450
 1620 Exam Winter 2021
---
+++ Scripts/app.js
@@ -0,0 +1,6 @@
+const albums = [
+    { name: "Album 1", price: 12.99, inCart: false }, 
+    { name: "Album 2", price: 14.99, inCart: false }, 
+    { name: "Album 3", price: 9.99, inCart: false }, 
+    { name: "Album 4", price: 19.99, inCart: false }
+  ];
